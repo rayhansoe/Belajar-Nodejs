fa46a2d441201448ebfc2820bb6f8461feb35253
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-28 16:12:11 +0700

    using yargs and mix with old way.

--- Contact App/app.js
+++ Contact App/app.js
@@ -1,12 +1,48 @@
-const { checkStatus, questions } = require('./contact')
+// local module
+const { checkStatus, simpanContact, questions } = require('./contact')
 
-// Main
-const main = async () => {
-	// Check Folder & File
-	checkStatus()
+// third party modules
+const yargs = require('yargs')
 
-	// the questions
-	questions()
-}
+// command add.
+yargs.command({
+	command: 'add',
+	describe: 'Menambahkan contact baru',
+	builder: {
+		nama: {
+			describe: 'Nama lengkap',
+			demandOption: true,
+			type: 'string',
+		},
+		email: {
+			describe: 'Email',
+			demandOption: false,
+			type: 'string',
+		},
+		noHp: {
+			describe: 'Nomor Handphone',
+			demandOption: true,
+			type: 'string',
+		},
+	},
+	handler(argv) {
+		// checkStatus / folder and file is exist.
+		checkStatus()
 
-main()
+		// simpan contact
+		simpanContact(argv.nama, argv.email, argv.noHp)
+	},
+})
+
+// commad run old way.
+yargs.command({
+	command: 'run',
+	describe: 'old way input contact',
+	handler(argv) {
+		// checkStatus / folder and file is exist.
+		checkStatus()
+		questions()
+	},
+})
+
+yargs.parse()
--- Contact App/contact.js
+++ Contact App/contact.js
@@ -2,6 +2,9 @@
 const fs = require('fs')
 const readline = require('readline')
 
+// third party modules
+const validator = require('validator')
+
 // vars
 const dirPath = 'data'
 const filePath = 'data/contacts.json'
@@ -33,33 +36,77 @@ const questionMaker = question => {
 	})
 }
 
-// tulis data
-const writeDataToFile = (data, nama) => {
-	fs.writeFileSync(filePath, JSON.stringify(data))
-	console.log(`Terima kasih ${nama}`)
+// simpan data
+const simpanContact = (nama, email, noHp, isDirect = true) => {
+	// target File
+	const targetFile = fs.readFileSync(filePath, 'utf-8')
+
+	// amanin data
+	const contact = { nama, email, noHp }
+
+	// from file raw parse to JSON
+	const contacts = JSON.parse(targetFile)
+
+	// check nama duplicate
+	const isDuplicate = contacts.find(contact => contact.nama === nama)
+	if (isDuplicate) {
+		console.log('Contact sudah terdaftar, gunakan nama lain!')
+		if (isDirect) {
+			closeApp()
+			return false
+		} else {
+			return false
+		}
+	}
+
+	// check email is valid
+	if (email) {
+		if (!validator.isEmail(email)) {
+			// console.log(chalk.red.inverse.bold('Email yang kamu masukan tidak valid!'))
+			console.log('Email yang kamu masukan tidak valid!')
+			if (isDirect) {
+				closeApp()
+				return false
+			} else {
+				return false
+			}
+		}
+	}
+
+	// check noHp is valid
+	if (!validator.isMobilePhone(noHp, 'id-ID')) {
+		// console.log(chalk.red.inverse.bold('Nomor yang kamu masukan tidak valid!'))
+		console.log('Nomor yang kamu masukan tidak valid!')
+		if (isDirect) {
+			closeApp()
+			return false
+		} else {
+			return false
+		}
+	}
+
+	// push data
+	contacts.push(contact)
+
+	// save data ke file
+	fs.writeFileSync(filePath, JSON.stringify(contacts))
+	console.log(`Terima kasih ${nama}. Contact berhasil dimasukan.`)
+
+	// jika direct di command line langsung tutup
+	if (isDirect) {
+		closeApp()
+	}
 }
 
 // Pertanyaan v1
 const questions = async () => {
-	// target File
-	const targetFile = fs.readFileSync(filePath, 'utf-8')
-
 	// QnA
 	const nama = await questionMaker('Masukan nama anda: ')
 	const email = await questionMaker('Masukan email anda: ')
 	const noHp = await questionMaker('Masukan no Hp anda: ')
 
-	// from file raw pase to JSON
-	const contacts = JSON.parse(targetFile)
-
-	// amanin data
-	const contact = { nama, email, noHp }
-
-	// push data
-	contacts.push(contact)
-
 	// tulis data / save data
-	writeDataToFile(contacts, nama)
+	simpanContact(nama, email, noHp, false)
 
 	// reAsk
 	reAsk()
@@ -89,4 +136,5 @@ const reAsk = async () => {
 module.exports = {
 	questions,
 	checkStatus,
+	simpanContact,
 }
